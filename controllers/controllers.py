341cf4f724922d52352919c8ba239e9de08c05fd
Ralf Vench Billones <ralf.toolkt@gmail.com>
2020-12-15 23:02:00 +0800

    fix var

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -75,8 +75,9 @@ class BaseRest(http.Controller):
             known_faces_names = []
             for user_w_profile in users:
                 known_faces.append(get_face_encoding_from_base64(user_w_profile.profile))
-                known_faces_names.append(user_w_profile.name)
+                known_face_names.append(user_w_profile.name)
             if len(image_receive) > 0:
+                print(known_faces_names)
                 for encoding in image_receive:                    
                     # image_user = get_face_encoding_from_base64(user.profile)                
                     result = fr.compare_faces(known_faces, encoding)
