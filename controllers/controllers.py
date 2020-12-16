ed66c2e41af747695fb409015500719c76aeed21
Ralf Vench Billones <ralf.toolkt@gmail.com>
2020-12-16 14:36:42 +0800

    print kw'image'

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -69,6 +69,7 @@ class BaseRest(http.Controller):
         response = {}
         user = request.env['res.users'].sudo().search([('token', '=', kw['token'])])
         if user:
+            print(kw['image'])
             image_receive = get_face_encoding_from_base64(kw['image'] + '======')
             users = request.env['res.users'].sudo().search([('active', '=', True), ('profile', '!=', False)])
             print(users)
